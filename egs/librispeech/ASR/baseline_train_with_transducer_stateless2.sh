ad589c6cbfe8d2061e2acacf34d932c71053e7b5
gipder <rimhsak@naver.com>
2023-04-10 04:40:00 +0000

    initial commit for baseline

---
+++ egs/librispeech/ASR/baseline_train_with_transducer_stateless2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+basedir="transducer_stateless2_baseline"
+task="baseline"
+#[ -e ${basedir}/${task} ] && rm -rf ${basedir}/${task}
+${basedir}/train.py \
+    --world-size 2 \
+    --master-port 13546 \
+    --num-epochs 30 \
+    --start-epoch 0 \
+    --exp-dir ${basedir}/${task} \
+    --full-libri 0 \
+    --max-duration 100 \
+    --lr-factor 2.5
