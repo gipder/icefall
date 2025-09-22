97efadc051e5485a3ff6376106304904e4d4258d
gipder <rimhsak@naver.com>
2025-09-22 14:09:19 +0900

    cleaning up AISHELL-1

---
+++ egs/aishell/ASR_category/pruned_transducer_stateless7_0806_bilinear/joiner.py
@@ -0,0 +1,81 @@
+# Copyright    2021  Xiaomi Corp.        (authors: Fangjun Kuang)
+#
+# See ../../../../LICENSE for clarification regarding multiple authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+import torch.nn as nn
+
+
+class Joiner(nn.Module):
+    def __init__(
+        self,
+        encoder_dim: int,
+        decoder_dim: int,
+        joiner_dim: int,
+        vocab_size: int,
+    ):
+        super().__init__()
+
+        self.encoder_proj = nn.Linear(encoder_dim, joiner_dim)
+        self.decoder_proj = nn.Linear(decoder_dim, joiner_dim)
+        self.w_joint1 = nn.Linear(joiner_dim, joiner_dim)
+        self.w_joint2 = nn.Linear(joiner_dim, joiner_dim)
+        self.w_low1_t = nn.Linear(joiner_dim, joiner_dim//2)
+        self.w_low2_t = nn.Linear(joiner_dim, joiner_dim//2)
+        self.w_proj = nn.Linear(joiner_dim//2, joiner_dim)
+        self.output_linear = nn.Linear(joiner_dim, vocab_size)
+
+    def forward(
+        self,
+        encoder_out: torch.Tensor,
+        decoder_out: torch.Tensor,
+        project_input: bool = True,
+    ) -> torch.Tensor:
+        """
+        Args:
+          encoder_out:
+            Output from the encoder. Its shape is (N, T, s_range, C).
+          decoder_out:
+            Output from the decoder. Its shape is (N, T, s_range, C).
+           project_input:
+            If true, apply input projections encoder_proj and decoder_proj.
+            If this is false, it is the user's responsibility to do this
+            manually.
+        Returns:
+          Return a tensor of shape (N, T, s_range, C).
+        """
+        assert encoder_out.ndim == decoder_out.ndim
+        assert encoder_out.ndim in (2, 4)
+        #print(f"{encoder_out.shape=}")
+        #print(f"{decoder_out.shape=}")
+
+        #if project_input:
+        #    logit = self.encoder_proj(encoder_out) + self.decoder_proj(decoder_out)
+            #print(f"{logit.shape=}")
+        #else:
+        #    logit = encoder_out + decoder_out
+
+        h_hat = self.w_proj(
+            torch.tanh(self.w_low1_t(encoder_out))
+            * torch.tanh(self.w_low2_t(decoder_out))
+        )
+
+        logit = self.output_linear(
+            torch.tanh(
+                h_hat + self.w_joint1(encoder_out) + self.w_joint2(decoder_out)
+            )
+        )
+
+        return logit
